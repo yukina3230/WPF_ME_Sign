e3967838d625932aecc06c744ce29b8ae3f8729a
71720 <SPG-IT010@spg-sportsgear.com>
2022-08-18 14:09:22 +0700

    commit-ch

---
+++ WPF_ME_Sign/Models/Helpers/EncryptHelper.cs
@@ -0,0 +1,23 @@
+﻿using OracleInternal.Secure.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using MD5 = System.Security.Cryptography.MD5;
+
+namespace WPF_ME_Sign.Models.Helpers
+{
+    public static class EncryptHelper
+    {
+        public static string EncodeString(string input)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            return Convert.ToHexString(hashBytes);
+        }
+    }
+}
---
+++ WPF_ME_Sign/ViewModels/Login/LoginViewModel.Commands.cs
@@ -0,0 +1,34 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_ME_Sign.Models.Services;
+
+namespace WPF_ME_Sign.ViewModels.Login
+{
+    public partial class LoginViewModel
+    {
+        //public ICommand LoginCommand { get; }
+        public RelayCommand<object> LoginCommand { get; }
+
+        private void LoginExecute()
+        {
+            LoginService loginService = new LoginService();
+
+            if (loginService.Login(UserId, Password))
+            {
+                MessageBox.Show("Success");
+            }
+            else
+            {
+                MessageBox.Show("Wrong User Id or Password");
+            }
+        }
+        private bool LoginCanExecute() => (!String.IsNullOrEmpty(UserId) && !String.IsNullOrEmpty(Password)) ? true : false;
+    }
+}
